676611a61ca2a96123254899bf374d1375afe873
Nicolau NP <mr1Normaliii@gmail.com>
2023-01-18 13:07:30 +0000

    update

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,27 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Noticia } from 'src/app/Models/Noticia';
+import { NoticiaService } from 'src/app/service/noticia.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit{
+title = "Criando Form"
+reactiveForm!:FormGroup
 
-registerForm!:FormGroup
-submitted= false
-
-constructor(private formBuilder:FormBuilder){}
+constructor(private noticiaService: NoticiaService) {
+}
 
 ngOnInit(){
-  this.registerForm = this.formBuilder.group({
-titulo: ['', Validators.required],
-descricao: ['', Validators.required],
-img: ['', Validators.required],
+  this.reactiveForm = new FormGroup({
+      titulo: new FormControl(null, Validators.required),
+      descricao:  new FormControl(null, Validators.required),
+      img:  new FormControl(null, Validators.required),
   })
 }
 
 onSubmit(){
+
+}
+
+storeNoticia() {
+  const noticia: Noticia = {
+    titulo: this.reactiveForm.value.titulo,
+    descricao: this.reactiveForm.value.descricao,
+    img: this.reactiveForm.value.img
+  }
+
+  this.noticiaService.store(noticia).subscribe(
+    response => {
+      console.log(response)
+    })
 
 }
 
