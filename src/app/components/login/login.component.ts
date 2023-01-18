66c363ff26362003c9d12ae52d45feed9636d927
Nicolau NP <mr1Normaliii@gmail.com>
2023-01-18 17:06:24 +0000

    trabalhando no login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NoticiaService } from 'src/app/service/noticia.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit{
   reactiveForm!:FormGroup
 
+  constructor(private noticiaService: NoticiaService){}
+
   ngOnInit(){
     this.reactiveForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
+      email: new FormControl(null,[Validators.required, Validators.email]),
       password:  new FormControl(null, Validators.required),
     })
   }
 
   onSubmit(){
-      alert("hello");
+      this.logar()
   }
+
+  logar(){
+    this.noticiaService.login(this.reactiveForm.value).subscribe(
+    (response:any) => {
+      console.log(response.data)
+    })
+  }
+
 
 }
