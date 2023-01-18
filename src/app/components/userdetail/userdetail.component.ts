9f3eb59aa031ec5c322e5a0b3c686b8ff6cbb1a1
Nicolau NP <mr1Normaliii@gmail.com>
2023-01-18 18:14:02 +0000

    update

--- src/app/components/userdetail/userdetail.component.ts
+++ src/app/components/userdetail/userdetail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-userdetail',
   templateUrl: './userdetail.component.html',
   styleUrls: ['./userdetail.component.css']
 })
-export class UserdetailComponent {
+export class UserdetailComponent implements OnInit{
+
+constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    const user:any = localStorage.getItem('user')
+    const userobj = JSON.parse(user)
+
+    const token = userobj.data.token
+
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer '+token,
+    })
+
+    this.http.get('http://127.0.0.1:8000/api/user', {headers: headers}).subscribe((response:any)=>{
+      console.log(response)
+    })
+
+  }
+
+
 
 }
